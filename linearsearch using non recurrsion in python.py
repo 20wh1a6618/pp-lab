d1b1b3dd2d6cd1eac195ca07a06e2e91594914b4
20wh1a6618 <93579936+20wh1a6618@users.noreply.github.com>
2021-11-09 18:45:42 +0530

    Add files via upload

---
+++ linearsearch using non recurrsion in python.py
@@ -0,0 +1,17 @@
+def linearsearch(arr,n,x):
+    for i in range(0,n):
+        if(arr[i] == x):
+            return i+1
+        return -1
+    n=int(input("enter the total number of element"))
+    arr = []
+    for i in range(o,n):
+        ele = int(input("enter the array elements"))
+        arr.append(ele)
+        print(arr)
+        x = int(input("enter the serach element"))
+        answer = linearsearch(arr,n,x)
+        if(answer == -1):
+            print("enter not found")
+        else:
+            print("element found at position:",answer)    
